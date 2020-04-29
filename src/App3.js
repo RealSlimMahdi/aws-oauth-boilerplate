2dc27fa18bf4a2058c91ba24c3b67e5ffb0818e8
RealSlimMahdi <RealSlimMahdi@users.noreply.github.com>
2020-04-29 19:21:34 +0200

    fix final issue

--- src/App3.js
+++ src/App3.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import GoogleLogin, { GoogleLogout } from "react-google-login";
 
 const App3 = () => {
-  const [userDetails, setUserDetails] = useState({});
+  const [userDetails, setUserDetails] = useState(null);
   const [isUserloggedIn, setIsUserloggedIn] = useState(false);
 
   // useEffect(() => {
@@ -10,30 +10,31 @@ const App3 = () => {
   // }, []);
 
   const responseGoogle = (response) => {
-    // let email = response.profileObj.email.toString().toLowerCase();
-    // let emaildomain = email.split("@")[1];
-    // let authorizedDomain = ["roche.com", "gmail.com"];
-    // let isAuthorizedDomain = authorizedDomain.includes(emaildomain);
-    // if (isAuthorizedDomain) {
-    //   setIsUserloggedIn(true);
-    //   setUserDetails(response.profileObj);
-    // }
-    // if (!isAuthorizedDomain) {
-    //   setIsUserloggedIn(false);
-    //   setUserDetails({});
-    // }
-    setIsUserloggedIn(true);
-    setUserDetails(response.profileObj);
+    let email = response.profileObj.email.toString().toLowerCase();
+    let emaildomain = email.split("@")[1];
+    let authorizedDomain = ["roche.com", "gmail.com"];
+    let isAuthorizedDomain = authorizedDomain.includes(emaildomain);
+    if (isAuthorizedDomain) {
+      setIsUserloggedIn(true);
+      setUserDetails(response.profileObj);
+    }
+    if (!isAuthorizedDomain) {
+      setIsUserloggedIn(false);
+      setUserDetails(null);
+    }
+    // console.log(response);
+    // setIsUserloggedIn(true);
+    // setUserDetails(response.profileObj);
   };
 
   const logout = () => {
     setIsUserloggedIn(false);
-    setUserDetails({});
+    setUserDetails(null);
   };
 
   return (
     <div>
-      {isUserloggedIn && <h3>Welcome mr {userDetails.givenName}</h3>}
+      {userDetails && <h3>Welcome mr {userDetails.givenName}</h3>}
       <GoogleLogin
         clientId={process.env.REACT_APP_GOOGLE_SECRET_CODE}
         fields="first_name,last_name,email,picture"
@@ -41,7 +42,7 @@ const App3 = () => {
         onFailure={responseGoogle}
         buttonText="LogIn"
         isSignedIn={true}
-        hostedDomain="gmail.com"
+        // hostedDomain="gmail.com"
       />
       <GoogleLogout buttonText="LogOut" onLogoutSuccess={logout} />
     </div>
